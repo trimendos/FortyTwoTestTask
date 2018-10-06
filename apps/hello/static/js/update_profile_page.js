e1b3e0f421135c7402a7651ab2210749e6278d8b
trimendos <trimendos@gmail.com>
2018-10-07 00:42:18 +0300

    5. Add js-function that will be displayed a preview of photo, and change preview picture if a user changed it. Add gif-animation that will indicate of loading state. Add temporary timer for testing display gif. Make disabling of form during submit, so nothing could be entered/changed there.

--- apps/hello/static/js/update_profile_page.js
+++ apps/hello/static/js/update_profile_page.js
@@ -17,4 +17,50 @@ $(function () {
         maxDate: '-16Y',
         yearRange : '1940:c'
     });
+
+    $("#photo").on("change", function () {
+        var input = this;
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader(),
+                picture = $(".picture");
+
+            reader.onload = function (e) {
+                picture.css("max-width", 200);
+                picture.css("max-height", 200);
+                picture.attr("src", e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    });
+
+    function disableEnableFields(flag) {
+        $("input, submit, textarea, button").attr("disabled", flag);
+        $("#back_link").toggleClass("disable-links");
+        $("#loading").toggle();
+        if(flag) {
+            $("#home-link").attr("aria-disabled", true).addClass("disabled")
+        } else {
+            $("#home-link").attr("aria-disabled", false).removeClass("disabled")
+        }
+    }
+    function simulateDataProcess() {
+        var profile = {};
+        disableEnableFields(true);
+        $("input:not(:hidden, :button, :submit), textarea").each(
+            function (i, el) {
+                profile[el.name] = el.value;
+            }
+        );
+        localStorage.setItem("profile", JSON.stringify(profile));
+        setTimeout(function () {
+            disableEnableFields(false);
+        }, 3000);
+    }
+
+    $("#save").on("click", function () {
+        simulateDataProcess();
+        return false;
+    });
+
 });
