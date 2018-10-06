48c5aa8980e2e1dc604ef58d667b36f2cb13bc2c
trimendos <trimendos@gmail.com>
2018-10-07 02:43:30 +0300

    the url fixed

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import patterns, include, url
-
+from django.contrib.flatpages.views import flatpage
 from django.contrib import admin
 
 from apps.hello.views import MainPageView, RequestsPageView
@@ -13,15 +13,11 @@ urlpatterns = patterns(
     url('^$', MainPageView.as_view(), name='main_page'),
     url(r'^requests_page/$', RequestsPageView.as_view(), name='requests_page'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r"^update_profile_page/$",
+        login_required(flatpage),
+        {'url': "/update_profile_page/"},
+        name="update_profile_page"),
     url(r"^accounts/", include("django.contrib.auth.urls",
                                namespace="accounts")),
 )
 urlpatterns += staticfiles_urlpatterns()
-
-urlpatterns += patterns(
-    'django.contrib.flatpages.views',
-    url(r'^update_profile_page/$',
-        login_required('flatpage'),
-        {'url': '/update_profile_page/'},
-        name='update_profile_page'),
-)
