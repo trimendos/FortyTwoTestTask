08fde8742d1874220e73d2a7766b12a2d7246a1b
trimendos <trimendos@gmail.com>
2018-10-06 21:27:12 +0300

    4. Add validation of input by bootstrap.

--- apps/hello/static/js/update_profile_page.js
+++ apps/hello/static/js/update_profile_page.js
@@ -1,7 +1,20 @@
 $(function () {
+    "use strict";
+
+    $("#profile").submit(function () {
+        if (this.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        this.classList.add("was-validated");
+
+    });
+
     $("#birthday").datepicker({
         changeMonth: true,
         changeYear: true,
-        dateFormat: "MM d, yy"
+        dateFormat: "dd/mm/yy",
+        maxDate: '-16Y',
+        yearRange : '1940:c'
     });
 });
