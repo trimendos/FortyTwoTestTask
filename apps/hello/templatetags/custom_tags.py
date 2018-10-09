da3e5fbb633f585f12fea159c3a8841db78e7943
trimendos <trimendos@gmail.com>
2018-10-09 15:23:00 +0300

    2. Code the template tag "edit_link object". Add it to the template.(tests pass)

--- apps/hello/templatetags/custom_tags.py
+++ apps/hello/templatetags/custom_tags.py
@@ -1,9 +1,16 @@
 from django import template
-
+from django.core.urlresolvers import reverse
 
 register = template.Library()
 
 
 @register.simple_tag
 def edit_link(obj):
-    pass
+    """A template tag accepts any object and renders a link to its admin edit
+    page"""
+    try:
+        view_name = 'admin:{}_change'.format(obj._meta.db_table)
+        return reverse(view_name, args=(obj.pk,))
+    except AttributeError:
+        raise template.TemplateSyntaxError(
+            "{} isn't correct object".format(obj))
