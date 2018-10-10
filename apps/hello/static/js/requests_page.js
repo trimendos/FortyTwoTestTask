20b2ddc0a4397c064ca7783a2509bf8a28a2e4d6
trimendos <trimendos@gmail.com>
2018-10-10 22:07:07 +0300

    3. Code JS handlers for change priority value, handling of errors and sort table by priority and DateTime field.

--- apps/hello/static/js/requests_page.js
+++ apps/hello/static/js/requests_page.js
@@ -5,18 +5,33 @@ app.Requests = function() {
 
     options = {
         row_tpl: "<tr><td>{method}</td><td>{url}</td><td>{status_code}</td><td>{datetime}</td><td data-id='{id}'><button class='btn btn-light priority'>{priority}</button></td></tr>",
+        priority_input: "<input type=number id=id_priority class=form-control max=999 min=1 value='{priority}'/>",
         title_msg: "Requests",
+        bidi_arrow: "fas fa-sort-amount-down",
 
         updateInterval: 1000,
 
+        priorityBtn: "button.priority",
+        sort_ico: "fas fa-sort-amount-up",
+        sort_ico_alt: "fas fa-sort-amount-down",
+
+
         tbody: $("tbody"),
-        title: $("title")
+        title: $("title"),
+        sortPriorityBtn: $("thead th.sort__priority"),
+        sortDatetimeBtn: $("thead th.sort__datetime")
+
     };
 
     store = {
+        isAutoUpdate: true,
         newRequests: 0,
         intervalId: 0,
-        isPageInFocus: false
+        isPageInFocus: false,
+        prioritySortType: "high",
+        isDatetimeSort: false,
+        datetimeSortType: "last",
+        forceUpdate: false
     };
 
     window.addEventListener("focus", handleOnFocus);
@@ -42,8 +57,14 @@ app.Requests = function() {
 
     function makeUrl(base) {
         base = base || window.location.href;
-
-        return base + "?infocus={infocus}".format({infocus: isFocus()});
+        return "{base}?infocus={infocus}&{sort_by}={sort}&force_update={force}"
+            .format({
+            base: base,
+            infocus: isFocus(),
+            sort_by: store.isDatetimeSort ? "sort_datetime" : "sort_priority",
+            sort: store.isDatetimeSort ? store.datetimeSortType : store.prioritySortType,
+            force: store.forceUpdate
+        });
     }
 
     function makeTemplate(args, template) {
@@ -62,29 +83,38 @@ app.Requests = function() {
         store.newRequests = newRequests;
 
         webrequests = response.webrequests;
-        webrequests.forEach(function (webrequest) {
+        if(webrequests){
+            webrequests.forEach(function (webrequest) {
 
-            html += makeTemplate({
-                "id": webrequest.id,
-                "datetime": webrequest.datetime,
-                "url": webrequest.url,
-                "status_code": webrequest.status_code,
-                "method": webrequest.method,
-                "priority": 0
+                html += makeTemplate({
+                    "id": webrequest.id,
+                    "datetime": webrequest.datetime,
+                    "url": webrequest.url,
+                    "status_code": webrequest.status_code,
+                    "method": webrequest.method,
+                    "priority": 0
+                });
             });
-        });
+        }
+
         // Update table
         insertRows(html);
+
+        // Bind buttons
+        priorityBtnHandler();
 
         // Update counter
         updateTitleOfPage();
 
     }
 
-    function makeRequest() {
+    function makeRequest(isForceUpdate) {
         let url;
 
+        isForceUpdate = isForceUpdate || false;
+        store.forceUpdate = isForceUpdate;
         url = makeUrl();
+        store.forceUpdate = false;
         $.getJSON(url, handleResponse);
     }
 
@@ -97,10 +127,151 @@ app.Requests = function() {
         options.updateInterval);
     }
 
+    function replaceBtnWithInput(btn) {
+        var priority = btn.text().trim(),
+            html = options.priority_input.format({priority: priority}),
+            input;
+        btn.hide();
+        btn.after(html);
+
+        input = $("input#id_priority")
+            .focus()
+            .blur(function (){
+                input.tooltip("destroy");
+                input.remove();
+                btn.show();
+            });
+        return input;
+    }
+
+    function sendInputtedData(input) {
+        var parent, data;
+        parent = $(input.parent());
+
+        data = {
+            priority: input.val(),
+            req_id: parent.attr("data-id")
+        };
+
+        $.ajax({
+            method: "POST",
+            data: data,
+            url: makeUrl(),
+
+            success: function (response) {
+                var errorDict = response.errors || false,
+                    joinedErrors = "",
+                    field,
+                    errors;
+
+                if (errorDict) {
+                    for (field in errorDict) {
+                        if (errorDict.hasOwnProperty(field)) {
+                            errors = errorDict[field].join(", ");
+                            joinedErrors += field + ": " + errors;
+                        }
+                    }
+
+                    input.tooltip({title: errors});
+                    input.tooltip("show");
+
+                } else {
+                    handleResponse(response);
+                }
+            },
+            error: function (x, s, m) {
+                input.tooltip({title: "Error! Message: " + m});
+                input.tooltip("show");
+            }
+        });
+    }
+
+    function priorityBtnHandler() {
+        // Add event on the priority button
+        $(options.priorityBtn).each(function () {
+            var btn = $(this),
+            keys = {
+                enter: 13,
+                up_arrow: 38,
+                down_arrow: 40
+            };
+
+            btn.click(function () {
+                var input = replaceBtnWithInput(btn);
+
+                input.keypress(function (e) {
+                    if (e.keyCode == keys.enter) {
+                        sendInputtedData(input);
+                    }
+                });
+                input.focusout(function () {
+                    sendInputtedData(input);
+                });
+                input.keyup(function (e) {
+                   if(e.keyCode == keys.up_arrow || e.keyCode == keys.down_arrow) {
+                       sendInputtedData(input);
+                   }
+                });
+            });
+        });
+    }
+
+    function toggleSort() {
+        options.sortPriorityBtn.click(function () {
+            console.log(store);
+            var self = $(this),
+                arrow = self.find("i"),
+                currClass = arrow.attr("class"),
+                sortIco = store.prioritySortType == "high" ? options.sort_ico : options.sort_ico_alt;
+
+            store.prioritySortType = store.prioritySortType == "high" ? "low" : "high";
+
+            store.isDatetimeSort = false;
+
+            options.sortDatetimeBtn.removeClass("link").addClass("text-muted");
+            options.sortDatetimeBtn.find("i").removeClass().addClass(options.bidi_arrow);
+
+            self.removeClass("text-muted").addClass("link");
+            arrow.removeClass(currClass).addClass(sortIco);
+
+            makeRequest(true);
+        });
+    }
+
+    function toggleDatetimeSort() {
+        options.sortDatetimeBtn.click(function () {
+            var self = $(this),
+                arrow = self.find("i"),
+                currClass = arrow.attr("class"),
+                sortIco = store.datetimeSortType == "last" ? options.sort_ico : options.sort_ico_alt;
+
+            store.datetimeSortType = store.datetimeSortType == "last" ? "first" : "last";
+            store.isDatetimeSort = true;
+
+            options.sortPriorityBtn.removeClass("link").addClass("text-muted");
+            options.sortPriorityBtn.find("i").removeClass().addClass(options.bidi_arrow);
+
+            self.removeClass("text-muted").addClass("link");
+            arrow.removeClass(currClass).addClass(sortIco);
+
+            makeRequest(true);
+        });
+    }
+
     function init() {
         // Turn auto update on
         autoUpdateOn();
         //
+        updateTitleOfPage();
+
+        priorityBtnHandler();
+
+        toggleDatetimeSort();
+        // Bind priority sort button
+        toggleSort();
+
+        app.Utils.setUpAjax();
+
         updateTitleOfPage();
     }
 
